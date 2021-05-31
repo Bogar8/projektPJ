b30b2d6b86205ef4c22c1744401138d5fdf58691
hozjan <truba.zan@gmail.com>
2021-05-31 19:02:21 +0200

    Dodana funkcija za dodajanje dostopa.

--- app/src/main/java/com/feri/projektpj/AddAccessActivity.kt
+++ app/src/main/java/com/feri/projektpj/AddAccessActivity.kt
@@ -2,36 +2,42 @@ package com.feri.projektpj
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.data.Mailbox
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 import java.util.*
-import java.util.Optional.empty
 
 class AddAccessActivity : AppCompatActivity() {
     private val TAG: String = this::class.java.simpleName
     val ACTIVITY_ID = 104
     val MAILBOX_CODE = "MAILBOX_CODE"
-
+    var mailboxCode: String? = null
+    private var apiPackage: String? = ""
     private var etUsername: EditText? = null
     private var tvDateFrom: TextView? = null
     private var tvDateTo: TextView? = null
     private var tvTimeFrom: TextView? = null
     private var tvTimeTo: TextView? = null
-    private var YearFrom: Int = 0
-    private var MonthFrom: Int = 0
-    private var DayFrom: Int = 0
-    private var HourFrom: Int = 0
-    private var MinuteFrom: Int = 0
-    private var YearTo: Int = 0
-    private var MonthTo: Int = 0
-    private var DayTo: Int = 0
-    private var HourTo: Int = 0
-    private var MinuteTo: Int = 0
+    private var yearFrom: Int = 0
+    private var monthFrom: Int = 0
+    private var dayFrom: Int = 0
+    private var hourFrom: Int = 0
+    private var minuteFrom: Int = 0
+    private var yearTo: Int = 0
+    private var monthTo: Int = 0
+    private var dayTo: Int = 0
+    private var hourTo: Int = 0
+    private var minuteTo: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,70 +47,78 @@ class AddAccessActivity : AppCompatActivity() {
         tvDateTo = findViewById(R.id.tvDateTo)
         tvTimeFrom = findViewById(R.id.tvTimeFrom)
         tvTimeTo = findViewById(R.id.tvTimeTo)
+        setCodeFromIntent()
+    }
+
+    fun setCodeFromIntent(){
+        val extras = intent.extras
+        if (extras != null) {
+            mailboxCode = extras.getString(MAILBOX_CODE)
+        }
     }
 
     fun onClickSelectDateAndTime(v: View) {
         //Opens date picker dialog for event date input
         if (v === tvDateFrom) {
             val c = Calendar.getInstance()
-            YearFrom = c[Calendar.YEAR]
-            MonthFrom = c[Calendar.MONTH]
-            DayFrom = c[Calendar.DAY_OF_MONTH]
+            yearFrom = c[Calendar.YEAR]
+            monthFrom = c[Calendar.MONTH]
+            dayFrom = c[Calendar.DAY_OF_MONTH]
             var tvDateFrom: TextView = findViewById(R.id.tvDateTo)
             val datePickerDialog = DatePickerDialog(this,
                 { view, year, monthOfYear, dayOfMonth -> tvDateFrom.setText(dayOfMonth.toString() + "." + (monthOfYear + 1) + "." + year) },
-                YearFrom,
-                MonthFrom,
-                DayFrom
+                yearFrom,
+                monthFrom,
+                dayFrom
             )
             datePickerDialog.show()
         }
         if (v === tvDateTo) {
             val c = Calendar.getInstance()
-            YearFrom = c[Calendar.YEAR]
-            MonthFrom = c[Calendar.MONTH]
-            DayFrom = c[Calendar.DAY_OF_MONTH]
+            yearFrom = c[Calendar.YEAR]
+            monthFrom = c[Calendar.MONTH]
+            dayFrom = c[Calendar.DAY_OF_MONTH]
             var tvDateTo: TextView = findViewById(R.id.tvDateTo)
             val datePickerDialog = DatePickerDialog(this,
                 { view, year, monthOfYear, dayOfMonth -> tvDateTo.setText(dayOfMonth.toString() + "." + (monthOfYear + 1) + "." + year) },
-                YearTo,
-                MonthTo,
-                DayTo
+                yearTo,
+                monthTo,
+                dayTo
             )
             datePickerDialog.show()
         }
         //Opens date picker dialog for event time input
         if (v === tvTimeFrom) {
             val c = Calendar.getInstance()
-            HourFrom = c[Calendar.HOUR_OF_DAY]
-            MinuteFrom = c[Calendar.MINUTE]
+            hourFrom = c[Calendar.HOUR_OF_DAY]
+            minuteFrom = c[Calendar.MINUTE]
             var tvTimeFrom: TextView = findViewById(R.id.tvTimeFrom)
             // Launch Time Picker Dialog
             val timePickerDialog = TimePickerDialog(this,
                 { view, hourOfDay, minute -> tvTimeFrom.setText(hourOfDay.toString() + ":" + minute) },
-                HourFrom,
-                MinuteFrom,
+                hourFrom,
+                minuteFrom,
                 true
             )
             timePickerDialog.show()
         }
         if (v === tvTimeTo) {
             val c = Calendar.getInstance()
-            HourFrom = c[Calendar.HOUR_OF_DAY]
-            MinuteFrom = c[Calendar.MINUTE]
+            hourFrom = c[Calendar.HOUR_OF_DAY]
+            minuteFrom = c[Calendar.MINUTE]
             var tvTimeTo: TextView = findViewById(R.id.tvTimeTo)
             // Launch Time Picker Dialog
             val timePickerDialog = TimePickerDialog(this,
                 { view, hourOfDay, minute -> tvTimeTo.setText(hourOfDay.toString() + ":" + minute) },
-                HourTo,
-                MinuteTo,
+                hourTo,
+                minuteTo,
                 true
             )
             timePickerDialog.show()
         }
     }
 
-    fun onClickAccess(view: View){
+    fun onClickAddAccess(view: View){
         try {
             if (TextUtils.isEmpty(etUsername?.getText())) {
                 etUsername?.setError(getString(R.string.empty))
@@ -126,7 +140,22 @@ class AddAccessActivity : AppCompatActivity() {
                 tvTimeTo?.setError(getString(R.string.empty))
                 return
             }
+            val calendarFrom = Calendar.getInstance()
+            val calendarTo = Calendar.getInstance()
 
+            calendarFrom.set(Calendar.YEAR, yearFrom)
+            calendarFrom.set(Calendar.MONTH, monthFrom)
+            calendarFrom.set(Calendar.DAY_OF_MONTH, dayFrom)
+            calendarFrom.set(Calendar.MINUTE, minuteFrom)
+            calendarFrom.set(Calendar.HOUR_OF_DAY, hourFrom)
+
+            calendarTo.set(Calendar.YEAR, yearTo)
+            calendarTo.set(Calendar.MONTH, monthTo)
+            calendarTo.set(Calendar.DAY_OF_MONTH, dayTo)
+            calendarTo.set(Calendar.MINUTE, minuteTo)
+            calendarTo.set(Calendar.HOUR_OF_DAY, hourTo)
+
+            addAccess(mailboxCode, calendarFrom.time.toString(), calendarTo.time.toString(), etUsername?.text.toString())
             etUsername?.setText("")
             tvDateFrom?.setText(R.string.date_from)
             tvDateTo?.setText(R.string.date_to)
@@ -136,5 +165,35 @@ class AddAccessActivity : AppCompatActivity() {
         }catch (ex: Exception){
             Toast.makeText(this, "check inserted values!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun addAccess(code: String?, dateFrom: String, dateTo: String, username: String) {
+        val formBody = FormBody.Builder()
+            .add("username", username)
+            .add("mailbox_code", code)
+            .add("date_from", dateFrom)
+            .add("date_to", dateTo)
+            .build()
+        val request: Request = Request.Builder()
+            .url("http://10.0.2.2:3000/packageAccess/api/create")
+            .post(formBody)
+            .build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, ex: IOException) {
+                Log.i(TAG, ex.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    apiPackage = it.body()?.string()
+                    val jsonObject = JSONObject(apiPackage)
+                    if (jsonObject.getBoolean("successful")) {
+                        Log.i(TAG, "Dostop dodan")
+                    }
+                }
+            }
+        })
+
     }
 }
